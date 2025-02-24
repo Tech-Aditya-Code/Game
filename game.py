9d8fa6aa11929e07cfeacc2eb2e283a0a4cd9454
Tech-Aditya-Code <techaditya36@gmail.com>
2025-02-24 07:57:24 +0530

    Files Added

---
+++ game.py
@@ -0,0 +1,26 @@
+import random
+import time
+
+
+message = "Guess a number between 1 to 10"
+
+for char in message:
+    print(char, end="", flush=True)
+    time.sleep(0.2)
+
+print("\n")
+
+def game():
+    n = int(input("Enter a number: "))
+
+    r = random.randint(1,10)
+    print("The number is: ",r)
+
+    
+    if (n == r):
+        print("you guess the right number!")
+    else:
+        print("you guess the wrong number!")
+    game()
+game()
+
